392388912752e7eb661b5ceac8d6354ba9a8515b
Mark Stoute <mark.stoute@loblaw.ca>
2021-10-04 22:41:22 -0400

    SFG-34

---
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgdi.controllers;
+
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("HelloWorld!!!");
+
+        return "Hi Folks";
+    }
+}
